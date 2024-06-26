a2bc00f4fc6637cdc0ac0b3f422cb8cd688f14f4
anish <anishmjain2003@gmail.com>
2024-06-26 12:35:06 +0530

    removed token frm the app.py file

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 import requests
 from flask import Flask,render_template,url_for
 from flask import request as req
-
+from dotenv import load_dotenv
+import os
+api_token = os.getenv('API_TOKEN')
 
 app = Flask(__name__)
+
+load_dotenv()
+
+host = os.getenv('FLASK_RUN_HOST', '127.0.0.1')
+port = int(os.getenv('FLASK_RUN_PORT', 5000))
+
+
+
+
 @app.route("/",methods=["GET","POST"])
 def Index():
     return render_template("index.html")
@@ -12,7 +23,7 @@ def Index():
 def Summarize():
     if req.method== "POST":
         API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
-        headers = {"Authorization": "Bearer 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"}
+        headers = {"Authorization": "Bearer ${api_token}"}
 
         data=req.form["data"]
 
@@ -33,5 +44,5 @@ def Summarize():
     
 
 if __name__ == '__main__':
-        app.debug=True
-        app.run()
+    app.debug = True
+    app.run(host=host, port=port)
